c74300bdae525d9d8dafa86bc1fdd1c202cf0b27
Kanokpich Chaiyawan <kanokpichchai29@gmail.com>
2021-09-01 23:42:14 +0700

    support a greater than b

--- app/controllers/ex2_controller.rb
+++ app/controllers/ex2_controller.rb
@@ -2,6 +2,13 @@ class Ex2Controller < ApplicationController
   def loop
     params[:a] = Integer(params[:a]) rescue nil
     params[:b] = Integer(params[:b]) rescue nil
+    if params[:a] != nil && params[:b] != nil
+      if params[:a] > params[:b]
+        temp = params[:a]
+        params[:a] = params[:b]
+        params[:b] = temp
+      end
+    end
     puts params[:a]
     puts params[:b]
   end
